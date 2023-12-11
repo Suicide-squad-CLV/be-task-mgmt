2b04fe1664987d1dac18d006e7d7ef902a15a9b1
sang.pham <sang.pham@cyberlogitec.com>
2023-12-11 20:41:21 +0700

    refactor: update be to load data into task-management ui

--- src/modules/task/task.service.ts
+++ src/modules/task/task.service.ts
@@ -2,14 +2,45 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import TaskEntity from './entities/task.entity';
 import { ILike, Repository } from 'typeorm';
-import { TaskFields, GRPCTask, GRPCTaskList } from 'src/protos/task';
+import {
+  TaskFields,
+  GRPCTask,
+  GRPCTaskList,
+  Empty,
+  GRPCStatusList,
+  GRPCStatus,
+} from 'src/protos/task';
+import StatusEntity from './entities/status.entity';
 
 @Injectable()
 export class TaskService {
   constructor(
     @InjectRepository(TaskEntity)
     private taskRepository: Repository<TaskEntity>,
+    @InjectRepository(StatusEntity)
+    private statusRepository: Repository<StatusEntity>,
   ) {}
+
+  async getAllStatus(request: Empty): Promise<GRPCStatusList> {
+    const condition: any = {
+      isDeleted: false,
+      ...request,
+    };
+    const statusList = await this.statusRepository.find({
+      where: condition,
+    });
+
+    if (statusList) {
+      // Map TaskEntity to GRPCTask
+      const result: GRPCStatus[] = statusList.map((status: StatusEntity) =>
+        status.toGRPCStatus(),
+      );
+      return { statusList: result };
+    }
+
+    // Throw error if there is no task
+    throw new HttpException('Can not find tasks', HttpStatus.NOT_FOUND);
+  }
 
   async getAllTasks(request: TaskFields): Promise<GRPCTaskList> {
     // Define condition for where clause based on request value
